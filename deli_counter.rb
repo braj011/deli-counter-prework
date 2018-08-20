27205fe04287f3b00804c43ddd1366d7095e7d4f
braj011 <bhagyaraj01@gmail.com>
2018-08-20 13:55:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,5 @@ end
 
 def take_a_number(katz_deli, name)
   line.push(name)  # or do line << name
-  puts "Welcome, #{name}. You are number #{line.length} in line.""
+  puts "Welcome, #{name}. You are number #{line.length} in line."
 end 
