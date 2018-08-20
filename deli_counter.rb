81a971db28604f2ece32867afef208b507fd2c6e
braj011 <bhagyaraj01@gmail.com>
2018-08-20 13:51:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+# function 1 
+
+def line(array) # this was the one I figured out
+  if array.length >= 1
+    newarray = []
+    counter = 1 
+    array.each do |name|
+      newarray.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{newarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+# function 2 
+
+def take_a_number
